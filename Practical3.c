70e425eab5885639f6eacc614295a16d4a888ca1
Billy Tran <quybanbk@gmail.com>
2020-03-12 21:25:23 +0700

    Updated the Practical3.c file

--- Practical3.c
+++ Practical3.c
@@ -1,23 +1,33 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
 int main() {
     while(1) {
     char str[100];
-    scanf("%[^\n]s",str);
-    printf("%s",str);
-    if (str == "quit") {
-        break;
+    printf("Please input value: ");
+    scanf("%s",str);
+        
+    if (strcmp(str,"quit") == 0) {
+        return 0;
     }
     printf("Main before fork() \n");
     int pid = fork();
     if (pid == 0) 
     {
-        printf("I am child after fork() \n");
-        char *args[] = {"run", "stop", "pause"};
-        execvp("run", args);
+        printf("I am child after fork() \n");        
+        char *argv[3];
+        argv[0] = "ls";
+        argv[1] = "-la";
+        argv[2] = NULL;
+        execvp(str, argv);
+        
     }    
-    else printf("I am parent after fork(), child is %d\n", pid);
-    Wait(NULL);
-    return 0;
+    else 
+    
+    {
+        printf("I am parent after fork(), child is %d\n", pid);
+        //wait(NULL);
     }
+    }
+
 }
